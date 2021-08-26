5dc1d4c6aff53640994580ec72fc185ca02c6818
Lehi Toskin <lehi.toskin@gmail.com>
2021-08-26 12:47:57 -0700

    Cleanup

--- Classing/Main.cs
+++ Classing/Main.cs
@@ -7,26 +7,9 @@ namespace Classing
     {
         static void Main()
         {
-            Laptop laptop = new(0, "mini", "Acer", "FreeBSD", 15);
-            // display generated laptop object
-            laptop.Display();
-
-            Console.WriteLine();
-
-            // rewrite laptop object
-            laptop.SetPartNumber(1);
-            laptop.SetModel("pro");
-            laptop.SetManufacturer("Toshiba");
-            laptop.SetSize(13);
-            laptop.SetOS("Windows 10");
-            // display modified object
-            laptop.Display();
-
-            //Console.WriteLine();
-
+            Laptop laptop = new(1, "pro", "Toshiba", "Windows 10", 13);
             Desktop desktop = new(2, "Giganto", "Alienware", "Windows 10",
                 new string[] { "BDR", "S/PDIF Card" });
-            desktop.Display();
 
             Console.WriteLine("DB stuff:");
 
@@ -43,21 +26,6 @@ namespace Classing
             db.DisplayTable(DB.Table.LAPTOP);
             Console.WriteLine("DB Desktop:");
             db.DisplayTable(DB.Table.DESKTOP);
-
-            // can throw an exception
-            Desktop d2 = (Desktop)db.GetPart(DB.Table.DESKTOP, 3);
-            if (d2 is not null)
-                d2.Display();
-
-            // delete single laptop object
-            //db.DeletePart(laptop);
-            // delete single desktop object
-            //db.DeletePart(desktop);
-
-            //Console.WriteLine("DB Laptop:");
-            //db.DisplayTable(DB.Table.LAPTOP);
-            //Console.WriteLine("DB Desktop:");
-            //db.DisplayTable(DB.Table.DESKTOP);
         }
     }
 }
