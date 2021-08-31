3a307cac1b0a4c3e746dcc35a3709c0ee6268707
Lehi Toskin <lehi.toskin@gmail.com>
2021-08-30 22:11:14 -0700

    Start on TUI

--- Classing/Main.cs
+++ Classing/Main.cs
@@ -5,14 +5,19 @@ namespace Classing
 {
     class Program
     {
+        // TODO: add part helper function (int pn, DB.Table table)
+        // TODO: delete part helper function (int pn, DB.Table table)
+        // TODO: get part helper function (int pn, DB.Table table)
+        // TODO: list table helper function (DB.Table table)
+
         static void Main()
         {
-            Laptop laptop = new(1, "pro", "Toshiba", "Windows 10", 13);
+            /*Laptop laptop = new(1, "pro", "Toshiba", "Windows 10", 13);
             Desktop desktop = new(2, "Giganto", "Alienware", "Windows 10",
                 new string[] { "BDR", "S/PDIF Card" });
             Tablet tablet = new(3, "Iconia", "Acer", "Android", 10, false);
 
-            Console.WriteLine("DB stuff:");
+            //Console.WriteLine("DB stuff:");
 
             // database connection
             DB db = new();
@@ -29,7 +34,23 @@ namespace Classing
             Console.WriteLine("DB Desktop:");
             db.DisplayTable(DB.Table.DESKTOP);
             Console.WriteLine("DB Tablet:");
-            db.DisplayTable(DB.Table.TABLET);
+            db.DisplayTable(DB.Table.TABLET);*/
+
+            string selection;
+            Console.WriteLine("\tInventory Management System");
+
+            do
+            {
+                Console.WriteLine("Selections:");
+                Console.WriteLine("1: Add New Part");
+                Console.WriteLine("2: Delete Part");
+                Console.WriteLine("3: Get Part By Number And Type");
+                Console.WriteLine("4: List A Table");
+                Console.WriteLine("0: Exit");
+                Console.Write("> ");
+                selection = Console.ReadLine();
+                Console.WriteLine("\nRead string \"{0}\"", selection);
+            } while (selection != "0");
         }
     }
 }
